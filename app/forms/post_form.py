9d004837cf077d671ddd03568c7e84f587a4314e
Nathan <nathanmac777@hotmail.com>
2021-07-30 23:01:42 +0000

    empty edit fixed, subfeedit post undefined fixed

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
-    content = TextAreaField("Content", validators=[DataRequired()])
+    content = TextAreaField("Content")
     userId = IntegerField("UserId")
     subfeeditId = IntegerField("SubfeeditId")
     time = StringField("Time")
